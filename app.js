786a000b60c50808cfe5500779ba352409fbb6fa
isabellanalu <isabellanalurodrigues@gmail.com>
2026-03-05 09:37:11 -0300

    Ja esta finalizado

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const numero1 = document.getElementById("nota1")
-const numero2 = document.getElementById ("nota2")
-const button = document.getElementById ("button")
-const resultado = document.getElementById ("resultado")
+const numero2 = document.getElementById("nota2")
+const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
 
-function calculaMedia (){
-    const nota1 = Number(numero1.value)
-    const nota2 = Number (numero2.value)
+function calculaMedia(){
+  const nota1 = Number(numero1.value)
+  const nota2 = Number(numero2.value)
 
-    const media = (nota1+nota2)/2
-    resultado.innerHTML =`a media e ${media}`
+  const media = (nota1 + nota2)/2
+
+  if (media < 5 ){
+    resultado.innerHTML = `sua media foi ${media.toFixed(2)}. Voce esta reprovado`
+  }else{
+    resultado.innerHTML = `sua media foi ${media.toFixed(2)}. parabens, voce esta aprovado`
+ 
+  }
 }
+
 button.addEventListener("click", calculaMedia)
